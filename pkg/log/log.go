60b7f80b13356163282b0e808dd898b021fba516
EAFA0 <TobiichiOrigami.d@gmail.com>
2022-07-03 12:28:27 +0000

    log 包字段调整

--- pkg/log/log.go
+++ pkg/log/log.go
@@ -7,27 +7,27 @@ import (
 type Filed = zap.Field
 
 var Int = func(key string, val int) Filed { return zap.Int(key, val) }
-var Err = func(err error) Filed { return String("errmsg", err.Error()) }
+var Err = func(err error) Filed { return String("msg", err.Error()) }
 var String = func(key string, val string) Filed { return zap.String(key, val) }
 
 var logger = zap.L()
 
-func Debug(msg string, fileds ...Filed) {
-	logger.Debug(msg, fileds...)
+func Debug(msg string, fields ...Filed) {
+	logger.Debug(msg, fields...)
 }
 
-func Info(msg string, fileds ...Filed) {
-	logger.Info(msg, fileds...)
+func Info(msg string, fields ...Filed) {
+	logger.Info(msg, fields...)
 }
 
-func Warn(msg string, fileds ...Filed) {
-	logger.Warn(msg, fileds...)
+func Warn(msg string, fields ...Filed) {
+	logger.Warn(msg, fields...)
 }
 
-func Error(msg string, fileds ...Filed) {
-	logger.Error(msg, fileds...)
+func Error(msg string, fields ...Filed) {
+	logger.Error(msg, fields...)
 }
 
-func Panic(msg string, fileds ...Filed) {
-	logger.Panic(msg, fileds...)
+func Panic(msg string, fields ...Filed) {
+	logger.Panic(msg, fields...)
 }
